07009f4590b21badce529b02d54bb4130cf93561
David V <davidvaz77@gmail.com>
2024-08-24 09:35:00 -0400

    Submitting the assignment after completing all the tasks

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -11,17 +11,32 @@ set -x
 mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
+
 ls 
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-touch file1 file2 file3 file4 file5
+
+ touch dir2/file1.txt dir2/file2.txt dir2/file3.txt dir2/file4.txt dir2/file5.txt
+ ls dir2
 
 # 4. Append the words "Hello world" to dir2/file3
 
+echo "Hello World" >> dir2/file3.txt
+
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
+
+cat dir2/file3.txt
 
 # 6. Delete file4
 
+ rm -ir dir2/file4.txt
+ ls
+
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 
+rm -ir dir4 dir5
+ls
+
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+
+ls
